e274b4ad863ceb6e325de1bc4dcf60fc9ab5afee
whitebird <whitebird@live.Be>
2015-12-24 15:20:26 +0100

    new database

--- LOB.py
+++ LOB.py
@@ -15,6 +15,7 @@ app.config.from_object(__name__)
 
 
 def init_db():
+    print "Initing database"
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
@@ -57,7 +58,7 @@ def add_name():
         response["add_name"] = True
     else:
         response["add_name"] = False
-    
+
     return json.dumps(response)
 
 @app.route('/get_lobby_file')
--- schema.sql
+++ schema.sql
@@ -1,6 +1,14 @@
-drop table if exists entries;
-create table entries (
+drop table if exists question;
+create table question (
   id integer primary key autoincrement,
-  title text not null,
-  text text not null
+  category text not null,
+  question text not null
+);
+
+drop table if exists fillers;
+create table fillers (
+  id integer primary key autoincrement,
+  questionid integer not null,
+  answer text not null,
+  FOREIGN KEY(questionid) REFERENCES question(id)
 );
